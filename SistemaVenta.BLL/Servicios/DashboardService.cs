ce0759a42247daf4e3211c712315c060094d064f
Charly <charly91.3ds@gmail.com>
2023-09-18 19:28:08 -0300

    terminada primera parte (APIs funcionando)

---
+++ SistemaVenta.BLL/Servicios/DashboardService.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class DashboardService : IDashBoardService
+    {
+        private readonly IVentaRepository _ventaRepositorio;
+        private readonly IGenericRepository<Producto> _productoRepositorio;
+        private readonly IMapper _mapper;
+
+        public DashboardService(IVentaRepository ventaRepositorio, IGenericRepository<Producto> productoaRepositorio, IMapper mapper)
+        {
+            _ventaRepositorio = ventaRepositorio;
+            _productoRepositorio = productoaRepositorio;
+            _mapper = mapper;
+        }
+
+        private IQueryable<Venta> retornarVentas(IQueryable<Venta> tablaVenta, int restarCantidadDias)
+        {
+            DateTime? ultimaFecha = tablaVenta.OrderByDescending(v => v.FechaRegistro).Select(v => v.FechaRegistro).First();
+
+            ultimaFecha = ultimaFecha.Value.AddDays(restarCantidadDias);
+
+            return tablaVenta.Where(v => v.FechaRegistro.Value.Date >= ultimaFecha.Value.Date);
+        }
+
+        private async Task<int> TotalVentasUltimaSemana()
+        {
+            int total = 0;
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if(_ventaQuery.Count() > 0)
+            {
+                var tablaVenta = retornarVentas(_ventaQuery, -7);
+                total = tablaVenta.Count();
+            }
+
+            return total;
+        }
+
+        private async Task<String> TotalIngresosUltimaSemana()
+        {
+            decimal resultado = 0;
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if (_ventaQuery.Count() > 0)
+            {
+                var tablaVenta = retornarVentas(_ventaQuery, -7);
+                resultado = tablaVenta.Select(v => v.Total).Sum(v => v.Value);
+            }
+
+            return Convert.ToString(resultado, new CultureInfo("es-AR"));
+        }
+
+        private async Task<int> TotalProductos()
+        {
+            IQueryable<Producto> _productoQuery = await _productoRepositorio.Consultar();
+
+            int total = _productoQuery.Count();
+            return total;
+        }
+
+        private async Task<Dictionary<String,int>> VentasUltimaSemana()
+        {
+            Dictionary<String, int> resultado = new Dictionary<String, int>();
+
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if (_ventaQuery.Count() > 0)
+            {
+                var tablaVenta = retornarVentas(_ventaQuery, -7);
+
+                resultado = tablaVenta
+                    .GroupBy(v => v.FechaRegistro.Value.Date)
+                    .OrderBy(g => g.Key)
+                    .Select(dv => new { fecha = dv.Key.ToString("dd/MM/yyyy"), total = dv.Count() })
+                    .ToDictionary(keySelector: r => r.fecha, elementSelector: r => r.total);
+            }
+
+            return resultado;
+        }
+
+        public async Task<DashboardDTO> Resumen()
+        {
+            DashboardDTO vmDashBoard = new DashboardDTO();
+
+            try
+            {
+                vmDashBoard.TotalVentas = await TotalVentasUltimaSemana();
+                vmDashBoard.TotalIngresos = await TotalIngresosUltimaSemana();
+                vmDashBoard.TotalProductos = await TotalProductos();
+
+                List<VentaSemanaDTO> listaVentaSemana = new List<VentaSemanaDTO>();
+
+                foreach(KeyValuePair<string,int> item in await VentasUltimaSemana())
+                {
+                    listaVentaSemana.Add(new VentaSemanaDTO { 
+                        Fecha = item.Key, 
+                        Total = item.Value 
+                    });
+                }
+
+                vmDashBoard.VentasUltimaSemana = listaVentaSemana;
+            }
+            catch
+            {
+                throw;
+            }
+
+            return vmDashBoard;
+        }
+    }
+}
---
+++ SistemaVenta.BLL/Servicios/ProductoService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class ProductoService : IProductoService
+    {
+        private readonly IGenericRepository<Producto> _productoRepositorio;
+        private readonly IMapper _mapper;
+
+        public ProductoService(IGenericRepository<Producto> productoRepositorio, IMapper mapper)
+        {
+            _productoRepositorio = productoRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductoDTO>> Lista()
+        {
+            try
+            {
+                var queryProducto = await _productoRepositorio.Consultar();
+
+                var listaProductos = queryProducto.Include(cat => cat.IdCategoriaNavigation).ToList();
+
+                return _mapper.Map<List<ProductoDTO>>(listaProductos.ToList());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<ProductoDTO> Crear(ProductoDTO modelo)
+        {
+            try
+            {
+                var productoCreado = await _productoRepositorio.Crear(_mapper.Map<Producto>(modelo));
+
+                if(productoCreado.IdProducto == 0)
+                {
+                    throw new TaskCanceledException("No se pudo crear el producto");
+                }
+
+                return _mapper.Map<ProductoDTO>(productoCreado);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(ProductoDTO modelo)
+        {
+            try
+            {
+                var productoModelo = _mapper.Map<Producto>(modelo);
+                var ProductoEncontrado = await _productoRepositorio.Obtener(p =>
+                p.IdProducto == productoModelo.IdProducto
+                );
+
+                if(ProductoEncontrado == null)
+                {
+                    throw new TaskCanceledException("No se encontro el producto a editar");
+                }
+
+                ProductoEncontrado.Nombre = productoModelo.Nombre;
+                ProductoEncontrado.IdCategoria = productoModelo.IdCategoria;
+                ProductoEncontrado.Stock = productoModelo.Stock;
+                ProductoEncontrado.Precio = productoModelo.Precio;
+                ProductoEncontrado.EsActivo = productoModelo.EsActivo;
+
+                bool respuesta = await _productoRepositorio.Editar(ProductoEncontrado);
+
+                if(!respuesta)
+                {
+                    throw new TaskCanceledException("No se pudo editar el producto");
+                }
+
+
+                return respuesta;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var productoEncontrado = await _productoRepositorio.Obtener(p =>
+                p.IdProducto == id
+                );
+
+                if(productoEncontrado == null)
+                {
+                    throw new TaskCanceledException("No se encontro el producto a eliminar");
+                }
+
+                bool respuesta = await _productoRepositorio.Eliminar(productoEncontrado);
+
+                if(!respuesta)
+                {
+                    throw new TaskCanceledException("No se pudo eliminar el producto");
+                }
+
+                return respuesta;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
