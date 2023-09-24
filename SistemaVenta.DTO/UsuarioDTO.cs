24bcaaabddb34ed3652789dc082428a25a7d16b6
Charly <charly91.3ds@gmail.com>
2023-09-24 13:17:56 -0300

    arreglado error en usuario DTO

--- SistemaVenta.DTO/UsuarioDTO.cs
+++ SistemaVenta.DTO/UsuarioDTO.cs
@@ -14,6 +14,7 @@ namespace SistemaVenta.DTO
 
         public string? Correo { get; set; }
 
+        public int? IdRol { get; set; }
         public string? RolDescripcion { get; set; }
 
         public string? Clave { get; set; }
